07d12489387d4249f05028bcf7863a1f4f1a02bd
khmal <khmaladznd@g,ail.com>
2026-02-09 12:36:27 +0400

    refactor test: add classes, refactor pages fixtures

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -7,65 +7,47 @@ from pages.shopping_cart_page import ShoppingCartPage
 
 
 @pytest.fixture
-def login_page(chromium_page: Page) -> LoginPage:
-    return LoginPage(page=chromium_page)
+def page(chromium_page: Page) -> Page:
+    """Базовая фикстура страницы с настройками"""
+    chromium_page.set_viewport_size({"width": 1920, "height": 1080})
+    chromium_page.goto("https://www.saucedemo.com/")
+    return chromium_page
 
 
 @pytest.fixture
-def products_page(chromium_page: Page) -> ProductsPage:
-    # Переходим на страницу логина
-    chromium_page.goto("https://www.saucedemo.com/")
+def login_page(page: Page) -> LoginPage:
+    """Фикстура страницы логина"""
+    return LoginPage(page=page)
 
-    # Выполняем логин
-    login_page = LoginPage(page=chromium_page)
-    login_page.fill_login_form(username='standard_user', password='secret_sauce')
-    login_page.click_login_button()
-
-    # Ждем перехода на страницу продуктов
-    chromium_page.wait_for_url("**/inventory.html")
-
-    return ProductsPage(page=chromium_page)
 
 @pytest.fixture
-def item_details_page(chromium_page: Page) -> ItemDetailsPage:
-    # Переходим на страницу логина
-    chromium_page.goto("https://www.saucedemo.com/")
-
-    # Выполняем логин
-    login_page = LoginPage(page=chromium_page)
+def logged_in_page(page: Page) -> Page:
+    """Фикстура для авторизованной страницы"""
+    login_page = LoginPage(page=page)
     login_page.fill_login_form(username='standard_user', password='secret_sauce')
     login_page.click_login_button()
+    page.wait_for_url("**/inventory.html")
+    return page
 
-    chromium_page.wait_for_url("**/inventory.html")
 
-    products_page = ProductsPage(page=chromium_page)
+@pytest.fixture
+def products_page(logged_in_page: Page) -> ProductsPage:
+    """Фикстура страницы продуктов"""
+    return ProductsPage(page=logged_in_page)
+
+
+@pytest.fixture
+def item_details_page(products_page: ProductsPage, page: Page) -> ItemDetailsPage:
+    """Фикстура страницы деталей товара"""
     products_page.item_link.click()
+    page.wait_for_url("**/inventory-item.html?id=*")
+    return ItemDetailsPage(page=page)
 
-    chromium_page.wait_for_url("**/inventory-item.html?id=3")
-
-    return ItemDetailsPage(page=chromium_page)
 
 @pytest.fixture
-def shopping_cart_page(chromium_page: Page) -> ShoppingCartPage:
-    # Переходим на страницу логина
-    chromium_page.goto("https://www.saucedemo.com/")
-
-    # Выполняем логин
-    login_page = LoginPage(page=chromium_page)
-    login_page.fill_login_form(username='standard_user', password='secret_sauce')
-    login_page.click_login_button()
-
-    chromium_page.wait_for_url("**/inventory.html")
-
-    products_page = ProductsPage(page=chromium_page)
-    products_page.item_link.click()
-
-    chromium_page.wait_for_url("**/inventory-item.html?id=3")
-
-    item_details_page = ItemDetailsPage(page=chromium_page)
+def shopping_cart_page(item_details_page: ItemDetailsPage, page: Page) -> ShoppingCartPage:
+    """Фикстура страницы корзины"""
     item_details_page.add_to_cart_button.click()
     item_details_page.shopping_cart.shopping_cart_link.click()
-
-    chromium_page.wait_for_url("**/cart.html")
-
-    return ShoppingCartPage(page=chromium_page)
+    page.wait_for_url("**/cart.html")
+    return ShoppingCartPage(page=page)
